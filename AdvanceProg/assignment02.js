3d40c93e40c728ca335e93f61c5f989199a7c1fb
Safaet <SafaetMDH@gmail.com>
2023-11-07 16:03:11 -0500

    Some Edits

--- AdvanceProg/assignment02.js
+++ AdvanceProg/assignment02.js
@@ -3,7 +3,6 @@
 var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
 
 function logDairy(){
-
     for(prop of dairy){
         console.log(prop);
     }
@@ -14,33 +13,29 @@ logDairy();
 //STEP 2
 
 const animal = {
-
+    
     canJump: true
-    
-    };
-    
-const bird = Object.create(animal);
-    
+}
+
+const bird = Object.create(animal)
     bird.canFly = true;
-    
     bird.hasFeathers = true;
 
-function birdCan(){
-
-    for(prop of Object.entries(bird)){
-        console.log(prop);
+function birdCan(){ 
+    for (elem of Object.keys(bird)) {   
+        console.log(`${elem}: ${bird[elem]}`); 
     }
-} 
-
-birdCan();
+}
+    
+birdCan();  
 
 //STEP 3
-
 
 function animalCan() { 
 
     for (prop in bird) {    
         console.log(`${prop}: ${bird[prop]}`);  
-    }}
+    }
+}
         
 animalCan(); 
