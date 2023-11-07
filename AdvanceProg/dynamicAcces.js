106d112d0cc062fb8bae219ccdff560d2ada0252
Safaet <SafaetMDH@gmail.com>
2023-11-07 10:43:41 -0500

    ADDS DRONE PROJECT

---
+++ AdvanceProg/dynamicAcces.js
@@ -0,0 +1,25 @@
+//Example of a Drone Project
+
+function testBracketsDynamicAccess() {
+    
+    let dynamicKey;
+
+    if(Math.random() > 0.5) {
+
+      dynamicKey = "speed";
+
+     }else{
+
+       dynamicKey = "color";
+
+     }
+  
+      let drone = {
+        speed: 15,
+        color: "orange"
+      }
+  
+      console.log(drone[dynamicKey]);
+  }
+
+  testBracketsDynamicAccess();
---
+++ AdvanceProg/forOf_objects.js
@@ -0,0 +1,17 @@
+const car = {
+    speed: 180,
+    color: 'Brooklyn Gray',
+    make : 'BMW',
+    model : 'x4',
+    year : 2023
+
+}
+
+//Method -> 'Object.keys()'
+console.log(Object.keys(car));
+
+//Method -> 'Object.values()'
+console.log(Object.values(car));
+
+//Method -> 'Object.entries'
+console.log(Object.entries(car));
